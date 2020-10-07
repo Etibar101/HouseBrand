c3a1879a190ab219d5e20ad25aa691aaf8ea55aa
etiw <esalahov11@gmail.com>
2020-10-07 12:54:26 +0400

    Updates

---
+++ precache-manifest.86eed4cfd785afdb78b4da5b67b042fe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea49ebaf49f9fe91c211eff1dc53aa8a",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "afab365ba0ef5c4a68bf",
+    "url": "/my-app/static/css/main.3ff7618b.chunk.css"
+  },
+  {
+    "revision": "1771f8e922dbbc34c8fa",
+    "url": "/my-app/static/js/2.5bc85551.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/my-app/static/js/2.5bc85551.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "afab365ba0ef5c4a68bf",
+    "url": "/my-app/static/js/main.a123f767.chunk.js"
+  },
+  {
+    "revision": "833d64ac4bae055be974",
+    "url": "/my-app/static/js/runtime-main.58a6d01e.js"
+  }
+]);
